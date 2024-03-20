2f0872f2372935ccf99d5fe448cd1034bb55f752
Andrey7967 <64693383+Andrey7967@users.noreply.github.com>
2024-03-20 22:50:21 +0300

    Add files via upload

---
+++ prnt/PRN1.cpp
@@ -0,0 +1,36 @@
+File name - d1.txt
+1 hello whatsup
+2 chop suye
+3  
+4 cool fish
+File name - d3.txt
+1 fghg
+2 hg
+3 h
+4 gh
+5 g
+6 ghg
+7 df
+8 d
+9 g
+10 g
+1 hg
+2 h
+3 gf
+4 hhg
+5 hg
+6 hg
+7 h
+8 hg
+9 hf
+10 gd
+1 fg
+2 
+File name - d2.txt
+1 rolton doshira
+2 
+3 
+4 
+5 foton electron
+6 
+7 
---
+++ prnt/lab 8.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream> // cout, cin
+#include <fstream> // ifstream, ofstream
+using namespace std;
+const
+int LPG = 10, // размер страницы
+LSTR = 80, // максимальная длина строки
+NLEN = 20; // максимальная длина имени файла
+ifstream Prg; // входной и
+ofstream List; // выходной файлы
+char Name[NLEN], // имя файла
+Chr; // рабочий символ
+int I = 1;
+int Len, NoExist = 0; // рабочие переменные
+int NextChar; // для обнаружения конца файла
+int numPage = 0;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	// открытие выходного файла
+	List.open("PRN1.cpp");
+	do // начало цикла входных файлов
+	{
+		cout << "\n Введите имя файла ";
+		cin >> Name;
+		Prg.open(Name);// открытие входного файла
+		I = 1;
+		if (Prg) // обработка, если файл существует
+		{
+			cout << "File name - " << Name << endl;
+			cout << I << " ";
+			List << "File name - " << Name << endl;
+			List << I << " ";
+			while (Prg.get(Chr)) // цикл страниц
+			{
+				cout << Chr;
+				List << Chr;
+				if (I == 10) {
+					I = 0;
+					cin.get(); // press enter to go next page
+
+				}
+				if (Chr == '\n') {
+					I++;
+					cout << I << " ";
+					List  << I << " ";
+				}
+				
+				
+				
+			}
+			cout << endl;
+			List << endl;
+			cout << "КОНЕЦ ВЫДАЧИ ОДНОГО ФАЙЛА";
+		}
+		else NoExist = 1;
+		Prg.close();
+	} while (!NoExist); // конец цикла входных файлов
+	List.close();
+	cout << "Прием файлов завершен, в связи с неверным именем входного файла" << endl;
+	system("pause");
+	return 0;
+}
